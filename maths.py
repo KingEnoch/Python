d88012992b83c4e19d6dab9cafa3f47de8c94139
D19126484 <D19126484@mytudublin.ie>
2019-10-17 15:33:52 +0100

    Example

---
+++ maths.py
@@ -0,0 +1,15 @@
+import math
+
+def gcd(a, b):
+    return math.gcd(a, b)
+def lcm(a, b):
+    return math.lcm(a, b)
+def addFrac(a, b):
+    return math.addFrac(a, b)
+def subFrac(a, b):
+    return math.subFrac(a, b)
+def reduce(a, b):
+    return math.reduce(a, b)
+
+num1 = tuple(input("Please enter the Number:"))
+
